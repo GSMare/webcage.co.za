fd5292d25df19c66cc885918c8a8307d5427ada7
gsmare <gerhard.mare@webcage.co.za>
2022-03-16 05:24:39 +0200

    Just an fresh update

--- src/App.js
+++ src/App.js
@@ -1,35 +1,44 @@
 import React, { Component } from 'react';
 
 import './App.css';
-import Flip from './components/flippy/Flip';
+
 import AppSlider from './components/slider/slider';
+import ProfilePic from './media/header_pp.png';
 
 class App extends Component {
   render() {
     return (
-     
+
       <div className="body">
         <header className="App">
-          <div>
-  
-                  <h1 id="h1" className="box-header">Gerhard Mare</h1>
-                  <p className="box-text1">WEB DEVELOPER / DESIGNER</p><br></br>
+            <div className="_box">
+              {/* <div className="card bg-transparent text-white"> */}
+                {/* <div className="card-img-overlay"> */}
+                  <h1 className="box-header">Gerhard Mare</h1>
+                  <p className="box-text1">WEB DEVELOPER / DESIGNER</p>
                   <p className="box-text2">Passionate about building meaningful experiences for users.</p>
                   <hr />
                   <hr />
-                  <a className="row justify-content-center" href="mailto:gerhard.mare@webcage.co.za"><button type="button" class="btn btn-outline-light">CONTACT ME</button></a>
-      
-          </div>
+                  <div style={{display: 'dlex', flexDirection: 'row', justifyContent: 'center', textAlign: 'center'}}>
+                   <a className="btn btn-outline-light" href="mailto:gerhard.mare@webcage.co.za">CONTACT ME</a>
+                  </div>
+              
+                {/* </div> */}
+              {/* </div> */}
+            </div>
         </header>
-        
+<footer>
+<img id='profilePic' src={ProfilePic} alt={'Gerhard Maré'} />
+</footer>
         <div id="container">
           <AppSlider />            
         </div>
-        <div id="container">
-          <Flip />            
-        </div>
+        
+
     
+
       </div>
+      
     );
   }
 }
--- src/components/slider/slider.js
+++ src/components/slider/slider.js
@@ -4,41 +4,55 @@ import Slider from 'react-animated-slider';
 import 'react-animated-slider/build/horizontal.css';
 import './slider-animations.css';
 import './styles.css';
+
 import Cube1 from '../cube/cube1';
 import Cube2 from '../cube2/cube2';
 import Cube3 from '../cube3/cube3';
+
 import Thumbsup from './media/Thumbsup.png';
+import header_pp from './media/header_pp.png';
 
 const content = [
 	{
 		Cube: <Cube1 />,
 		title: 'User Interface / User Experience',
-		description:
-		'I design and develop cross-platform design concepts for the full potential of your website. My passion lies in crafting an emersive UX through beautiful UI & interactions.',
+		description: [
+		'I design and develop cross-platform design concepts for the full potential of your website. My passion lies in crafting an emersive UX through beautiful UI & interactions.'],
 		button: 'Read More',
 		image: '',
-		user: 'UI/UX',
-		//userProfile: 'https://serv13.registerdomain.co.za:2083/cpsess5107406925/viewer/home%2fwebcagec%2fpublic_html%2fcgi-bin/Thumbsup.png',
+		heading: 'UI/UX',
+
 	},
 	{
 		Cube: <Cube2 />,
 		title: 'Tools & Skills',
-		description:
-		'Clean and sustainable. All sites are coded with performance and functionality in mind, no matter if it`s a small business landing page or large web application, the best practices and tools are used.',
+		description: [
+		'Clean and sustainable. All sites are coded with performance and functionality in mind, no matter if it`s a small business landing page or large web application, the best practices and tools are used.'],
 		button: 'Discover',
 		image: '',
-		user: 'Web Development',
-		//userProfile: 'https://serv13.registerdomain.co.za:2083/cpsess5107406925/viewer/home%2fwebcagec%2fpublic_html%2fcgi-bin/Thumbsup.png',
+		heading: 'Web Development',
+
 	},
 	{
 		Cube: <Cube3 />,
 		title: 'Mobile Applications',
-		description:
-		'I use React Native which is a Javascript framework to creact robust IOS and Android Apps. If you have a great idea for an App, please click on the "CONTACT ME" button and lets discuss your App`s possibilities.',
+		description: [
+		'I use React Native which is a Javascript framework to creact robust IOS and Android Apps. If you have a great idea for an App, please click on the "CONTACT ME" button and lets discuss your App`s possibilities.'],
 		button: 'CONTACT ME',
 		image: '',
-		user: 'App Development',
-		//userProfile: 'https://serv13.registerdomain.co.za:2083/cpsess5107406925/viewer/home%2fwebcagec%2fpublic_html%2fcgi-bin/Thumbsup.png',
+		heading: 'App Development',
+	},
+	{
+		Cube: header_pp.png,
+		title: 'About Me',
+		description: [
+		'I am a very curious person and also sure that is the reason why I started my career in IT because there is always samething new to learn.',
+		'My career started as a technician in IT and Office Automation and slowly converted as a Web Developer over time as I got more and more interested and finally found something I love.' , 
+		'Accept for as much as I enjoy spending time behind my computer, I enjoy the outdoors and endurance sport just as much.</p>'
+],
+		button: 'CONTACT ME',
+		image: '',
+		heading: 'Gerhard Mare',
 	}
 ];
 
@@ -55,13 +69,24 @@ const AppSlider = props => {
 						<div className="inner">
 							<h1>{item.title}</h1>
 							<p>{item.Cube}</p>
-							<p>{item.description}</p>
+							<p>{item.description[0]}</p>
+							<p>{item.description[1]}</p>
+							<p>{item.description[2]}</p>
 						</div>
+
+						<section>
+							<img src={Thumbsup} alt={item.heading} />
+							<span>
+								<strong>{item.heading}</strong>
+							</span>
+						</section>
+
 						<section style={{marginBottom: 0}}>
-							<img src={Thumbsup} alt={item.user} />
+							<img src={Thumbsup} alt={item.heading} />
 							
 						</section>
 						
+
 					</div>
 				))}
 			</Slider>
